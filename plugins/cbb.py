f69b2d1743a83955bb63e96a11d6d6a752201fbc
bipin8987 <107696698+bipin8987@users.noreply.github.com>
2022-06-21 00:49:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Bipin</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/bipin8987/advance-File-Sharing-Bot.git'>Click here</a>\n○ Channel : Bipin_Movies\n○ Support Group : @Bipin_Movies_Group</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Bipin</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/bipin8987/advance-File-Sharing-Bot.git'>Click here</a>\n○ Channel : @Bipin_Movies\n○ Support Group : @Bipin_Movies_Group</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
